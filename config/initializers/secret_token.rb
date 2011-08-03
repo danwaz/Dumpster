2a7e7d68bfc2ff3b387568c2fb76817fec641a71
Dan Wasilewski <heydanwaz@gmail.com>
2011-08-03 12:33:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dumpster::Application.config.secret_token = 'a4a64aaef3b2933746c31475234ef924383f2706175ac692364ebba31f2914f58b8526a26efea5b4075f4bc8d88213889bc852aa7f972d897d1605b3c6423d42'
