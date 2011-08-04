3fda52df77753d92a1d23c543469e85db7dd787b
Dan Wasilewski <heydanwaz@gmail.com>
2011-08-04 13:15:09 -0400

    Fixed edit/update for items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ActiveRecord::Base
-   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                                          :url => "/images/items/:id/:style/:basename.:extension",
+                                          :path => ":rails_root/public/images/items/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :image
+
 end
