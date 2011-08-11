05d006d42373f310dadc97490aaae18819b28e7a
Dan Wasilewski <heydanwaz@gmail.com>
2011-08-11 15:30:25 -0400

    removed config option

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.gem "rmagick",
-:lib => "RMagick" 
-
 # Initialize the rails application
 Dumpster::Application.initialize!
