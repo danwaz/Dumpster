9b186eaef51d6ccb40f8a33cfeb2488cb71ed850
Dan Wasilewski <heydanwaz@gmail.com>
2011-08-11 15:28:45 -0400

    added rmagick to config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.gem "rmagick",
+:lib => "RMagick" 
+
 # Initialize the rails application
 Dumpster::Application.initialize!
